14f7d8d566972d16e8b445440bf6fd0c3d176d2e
daniel-m10 <daniel.montecinos@jalasoft.com>
2020-05-08 20:04:59 -0400

    initial configuration

---
+++ src/main/java/org/techgroup/cinthia_argote/tic_tac_toe/Main.java
@@ -0,0 +1,9 @@
+package org.techgroup.cinthia_argote.tic_tac_toe;
+
+public class Main {
+  public static void main(String [] args){
+    System.out.println("Game Tic Tac Toe");
+    TicTacToe ticTacToe = new TicTacToe();
+    ticTacToe.playGame();
+   }
+}
---
+++ src/main/java/org/techgroup/cinthia_argote/tic_tac_toe/Player.java
@@ -0,0 +1,17 @@
+package org.techgroup.cinthia_argote.tic_tac_toe;
+
+public class Player {
+  String name;
+  int attempts;
+  char card;
+
+  Player(String name, char card) {
+    this.name = name;
+    this.card = card;
+    attempts = 5;
+  }
+
+  public void setAttempts(int attempts) {
+    this.attempts = attempts;
+  }
+}
